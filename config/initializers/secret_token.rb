565a8d559b2fc15047ccbfb46be8adc8045d7862
Deploy User <deploy@xen010-vm02.opnet.com>
2011-09-27 16:17:23 -0400

    initial commit rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruby19::Application.config.secret_token = 'e5b3a95dea7e60eb33fd778ddc2ad48f00af31f2e31b75d772814944fe6ec999bba4d7117b389ef27bb177624e0127674d255af6ff429133e492982380fdc8d1'
